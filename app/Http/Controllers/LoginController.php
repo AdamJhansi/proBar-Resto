17b92d3e401aca1cadb98b5629745888f010d2bc
Adam Jhansi <adamjhansi03@gmail.com>
2024-09-23 19:01:54 +0700

    up login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //
+
+    public function auth(){
+        // ini gua udah buat, coba lu tari
+    }
+}
